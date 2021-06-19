ba55f0cc68a974abb1df516362510bc5bc62f912
Rakesh Sharma <Rakesh Sharma@LAPTOP-AKSMRTPR>
2021-06-19 15:23:14 +0530

    user2 this is my first commit

--- com.crm.USER1/src/test/java/com/crm/com/crm/USER1/User1.java
+++ com.crm.USER1/src/test/java/com/crm/com/crm/USER1/User1.java
@@ -7,5 +7,6 @@ public class User1 {
 		System.out.println("i am user 1");
 		System.out.println("hi this is user1 modified");
 		System.out.println("hi am user1");
+		System.out.println("this is modified");
 	}
 }
