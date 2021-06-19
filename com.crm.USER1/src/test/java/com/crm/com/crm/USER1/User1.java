e1c40d7e73781ccf7d116e3f82baac0c4cb21d81
Rakesh Sharma <Rakesh Sharma@LAPTOP-AKSMRTPR>
2021-06-19 15:18:59 +0530

    user1 this is my scond commit

--- com.crm.USER1/src/test/java/com/crm/com/crm/USER1/User1.java
+++ com.crm.USER1/src/test/java/com/crm/com/crm/USER1/User1.java
@@ -5,5 +5,7 @@ package com.crm.com.crm.USER1;
 public class User1 {
 	public static void main(String[] args) {
 		System.out.println("i am user 1");
+		System.out.println("hi this is user1 modified");
+		System.out.println("hi am user1");
 	}
 }
